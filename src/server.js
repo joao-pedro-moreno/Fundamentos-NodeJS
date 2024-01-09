0263a73e597b47786e36ff337159f56c14aa2be5
João Pedro Moreno <devmoreno2003@gmail.com>
2024-01-08 21:05:38 -0300

    Streams no Node JS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js"
 
 // - HTTP
 //  - Método HTTP
@@ -28,21 +29,23 @@ import http from "node:http"
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  await json(req, res)
 
   if (method === "GET" && url === "/users") {
     // Early return (Caso o código execute o "return", nenhum código abaixo é executado)
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com"
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
